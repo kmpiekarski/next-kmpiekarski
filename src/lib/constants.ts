11a7099103742328c5025ac71cd9a2dee6e2ee7b
kmpiekarski <kenneth@kmpiekarski.com>
2024-03-11 15:28:04 -0400

    Connected contentful to pull from  "works" content model

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const SITE_URL="https://www.kmpiekarski.com"
-export const EXAMPLE_PATH = "cms-contentful";
-export const CMS_NAME = "Contentful";
-export const CMS_URL = "https://www.contentful.com";
+export const SITE_URL = 'https://www.kmpiekarski.com'
+export const EXAMPLE_PATH = 'cms-contentful'
+export const CMS_NAME = 'Contentful'
+export const CMS_URL = 'https://www.contentful.com'
