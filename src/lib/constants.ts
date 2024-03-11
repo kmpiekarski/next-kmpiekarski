953e2ba52f98bd2a4f833ee28ddb26b95ec62081
kmpiekarski <kenneth@kmpiekarski.com>
2024-03-11 14:46:49 -0400

    created constants file

---
+++ src/lib/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_URL="https://www.kmpiekarski.com"
+export const EXAMPLE_PATH = "cms-contentful";
+export const CMS_NAME = "Contentful";
+export const CMS_URL = "https://www.contentful.com";
